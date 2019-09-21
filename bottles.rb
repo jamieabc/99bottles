298eab45fbddbb8a6c3ab19f104463887e2d03a8
Aaron Chang <xesuschang@gmail.com>
2019-09-22 00:20:33 +0800

    fix rubocop warnings

--- bottles.rb
+++ bottles.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# Bottles - 99 bottles lyrics
 class Bottles
   def verse(number)
-    "#{number} bottles of beer on the wall, " +
-    "#{number} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{number - 1} #{pluralize(number)} of beer on the wall.\n"
+    "#{number} bottles of beer on the wall, " \
+      "#{number} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{number - 1} #{pluralize(number)} of beer on the wall.\n"
   end
 
   def pluralize(number)
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -5,26 +5,26 @@ require_relative '../bottles'
 
 class BottlesTest < Minitest::Test
   def test_the_first_verse
-    expected = "99 bottles of beer on the wall, " +
-        "99 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "98 bottles of beer on the wall.\n"
+    expected = '99 bottles of beer on the wall, ' \
+               "99 bottles of beer.\n" \
+               'Take one down and pass it around, ' \
+               "98 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(99)
   end
 
   def test_another_verse
-    expected = "3 bottles of beer on the wall, " +
-        "3 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "2 bottles of beer on the wall.\n"
+    expected = '3 bottles of beer on the wall, ' \
+               "3 bottles of beer.\n" \
+               'Take one down and pass it around, ' \
+               "2 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(3)
   end
 
   def test_verse_2
-    expected = "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n"
+    expected = '2 bottles of beer on the wall, ' \
+               "2 bottles of beer.\n" \
+               'Take one down and pass it around, ' \
+               "1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
   end
 end
