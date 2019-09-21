f3a7a6c15b1ffb0bf04e30951132a9dab6843a71
Aaron Chang <xesuschang@gmail.com>
2019-09-21 15:18:34 +0800

    first test

---
+++ bottles.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Bottles
+  def verse(_)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
+  end
+end
