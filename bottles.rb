e0e33fa83a611f27c9ad67a0fed248efb0bee0c5
Aaron Chang <xesuschang@gmail.com>
2019-09-30 15:34:30 +0800

    reafactor by small steps

--- bottles.rb
+++ bottles.rb
@@ -23,25 +23,19 @@ class Bottles
             "1 bottle of beer.\n" \
             "Take it down and pass it around, " \
             "no more bottles of beer on the wall.\n"
-          when 2
-            "2 bottles of beer on the wall, " \
-            "2 bottles of beer.\n" \
-            "Take one down and pass it around, " \
-            "1 bottle of beer on the wall.\n"
           else
             "#{number} bottles of beer on the wall, " \
             "#{number} bottles of beer.\n" \
             "Take one down and pass it around, " \
-            "#{number - 1} bottles of beer on the wall.\n"
+            "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
           end
 
     str
   end
-# use class for each bottle, default to most general
-# for 0, 1, 2, 6, and 7, with special returns
-# is this a good abstraction? I think naturally it's same as case, just use object
-# to encaspulate changes
-#
-# use string substitution for all words, 6 bottles => 1 six-pack
 
+  def container(number)
+    return 'bottle' if number == 1
+
+    'bottles'
+  end
 end
